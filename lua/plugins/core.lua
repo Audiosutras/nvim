181b7c5e005ebd35733034010be5df0c3d04b6ec
Christopher Dixon <christopher.snipes.dixon@gmail.com>
2024-03-08 11:16:25 -0500

    update core.lua

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,14 +1,31 @@
 return {
   -- icons
   { "nvim-tree/nvim-web-devicons", lazy = true },
-
   -- colorschemes
   { "projek0n/github-nvim-theme" },
-
+  -- Neotree additional configuration
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            ".git",
+          },
+          never_show = {},
+        },
+      },
+    },
+  },
+  -- LazyVim additional configuration
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_dark_high_contrast",
+      colorscheme = "github_dark_default",
     },
   },
 }
