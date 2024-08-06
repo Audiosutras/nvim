4d9e76b59eca6a33b390bfb8cfed4e5ad648b021
Christopher Dixon <christopher.snipes.dixon@gmail.com>
2024-08-05 21:50:15 -0400

    change colorscheme

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -4,10 +4,26 @@ return {
   { "nvim-tree/nvim-web-devicons", lazy = true },
   -- colorschemes
   { "EdenEast/nightfox.nvim" },
+  {
+    "0xstepit/flow.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("flow").setup({
+        transparent = true, -- Set transparent background.
+        fluo_color = "green", --  Fluo color: pink, yellow, orange, or green.
+        mode = "bright", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+        aggressive_spell = false, -- Display colors for spell check.
+      })
+
+      vim.cmd("colorscheme flow")
+    end,
+  },
   -- transparency plugin for transparent backgrounds
-  { "xiyaowong/transparent.nvim", lazy = false, opts = {
-    extra_groups = { "NeoTreeNormal" },
-  } },
+  -- { "xiyaowong/transparent.nvim", lazy = false, opts = {
+  --   extra_groups = { "NeoTreeNormal" },
+  -- } },
   -- Neotree additional configuration
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -33,7 +49,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "carbonfox",
+      colorscheme = "flow",
     },
   },
 }
