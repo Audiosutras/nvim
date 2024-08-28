a9eebef88dbc714aea010cf206878e666b10aa56
Christopher Dixon <christopher.snipes.dixon@gmail.com>
2024-08-27 23:04:52 -0400

    main: update

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -11,19 +11,15 @@ return {
     opts = {},
     config = function()
       require("flow").setup({
-        transparent = true, -- Set transparent background.
+        transparent = false, -- Set transparent background.
         fluo_color = "green", --  Fluo color: pink, yellow, orange, or green.
-        mode = "bright", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+        mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
         aggressive_spell = false, -- Display colors for spell check.
       })
 
       vim.cmd("colorscheme flow")
     end,
   },
-  -- transparency plugin for transparent backgrounds
-  -- { "xiyaowong/transparent.nvim", lazy = false, opts = {
-  --   extra_groups = { "NeoTreeNormal" },
-  -- } },
   -- Neotree additional configuration
   {
     "nvim-neo-tree/neo-tree.nvim",
