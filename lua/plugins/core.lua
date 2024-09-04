82637d9478c3b6d09e4a27e5d136839cbd5ab349
Christopher Dixon <christopher.snipes.dixon@gmail.com>
2024-09-04 00:40:22 -0400

    update

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -11,7 +11,7 @@ return {
     opts = {},
     config = function()
       require("flow").setup({
-        transparent = false, -- Set transparent background.
+        transparent = true, -- Set transparent background.
         fluo_color = "green", --  Fluo color: pink, yellow, orange, or green.
         mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
         aggressive_spell = false, -- Display colors for spell check.
