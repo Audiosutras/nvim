787c837b942e208fef54ea70c175d5b719df7190
Christopher Dixon <christopher.snipes.dixon@gmail.com>
2024-09-22 18:34:41 -0400

    main: refactor plugins dir

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,47 +1,5 @@
 -- https://www.lazyvim.org/configuration/plugins
 return {
-  -- icons
-  { "nvim-tree/nvim-web-devicons", lazy = true },
-  -- colorschemes
-  { "EdenEast/nightfox.nvim" },
-  {
-    "0xstepit/flow.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      require("flow").setup({
-        transparent = true, -- Set transparent background.
-        fluo_color = "green", --  Fluo color: pink, yellow, orange, or green.
-        mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
-        aggressive_spell = false, -- Display colors for spell check.
-      })
-
-      vim.cmd("colorscheme flow")
-    end,
-  },
-  -- Neotree additional configuration
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      window = {
-        position = "left",
-      },
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          show_hidden_count = true,
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          hide_by_name = {
-            ".git",
-          },
-          never_show = {},
-        },
-      },
-    },
-  },
-  -- LazyVim additional configuration
   {
     "LazyVim/LazyVim",
     opts = {
---
+++ lua/plugins/icons.lua
@@ -0,0 +1,5 @@
+-- https://www.lazyvim.org/configuration/plugins
+return {
+  -- icons
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+}
