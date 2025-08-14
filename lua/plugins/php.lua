2d1c1f9d8bdd73397bf651c42779cd577b4bbd40
Christopher Dixon <christopher.snipes.dixon@gmail.com>
2025-08-14 18:01:25 -0400

    add php config

---
+++ lua/plugins/php.lua
@@ -0,0 +1,48 @@
+-- https://seankegel.com/neovim-for-php-and-laravel#heading-lazyvim-php-extras
+return {
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { { "pint", "php_cs_fixer" } },
+      },
+    },
+  },
+  {
+    -- Add neotest-pest plugin for running PHP tests.
+    -- A package is also available for PHPUnit if needed.
+    -- <leader>t
+    "nvim-neotest/neotest",
+    dependencies = { "V13Axel/neotest-pest" },
+    opts = { adapters = { "neotest-pest" } },
+  },
+  {
+    -- Add a Treesitter parser for Laravel Blade to provide Blade syntax highlighting.
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "blade",
+        "php_only",
+      })
+    end,
+    config = function(_, opts)
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
+
+      require("nvim-treesitter.configs").setup(opts)
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+    end,
+  },
+}
